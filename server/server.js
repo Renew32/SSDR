9564be04e317cb7868268d12c75fd7c0e0950252
Antoine Missoup Konga <mcblum19@yahoo.com>
2025-06-07 21:09:51 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,13 +8,9 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2022-08-01",
 });
 
-app.use(express.static(process.env.STATIC_DIR));
+
 app.use(express.json()); // ← obligatoire !
 
-app.get("/", (req, res) => {
-  const path = resolve(process.env.STATIC_DIR + "/index.html");
-  res.sendFile(path);
-});
 
 app.get("/config", (req, res) => {
   res.send({
